3538746cc6baf6d751450b1c74500ffc632c3a68
Lidi Zheng <lidiz@google.com>
2019-07-22 14:09:15 -0700

    bug fix

--- tools/run_tests/performance/k8s/build.sh
+++ tools/run_tests/performance/k8s/build.sh
@@ -1,11 +1,14 @@
 #! /bin/bash -ex
 
-mkdir -p tools/dockerfile/benchmark/cpp_worker/build
-docker build -t cpp-builder tools/dockerfile/benchmark/cpp_worker
+mkdir -p tools/dockerfile/benchmark/cpp_builder/build
+docker build -t cpp-builder tools/dockerfile/benchmark/cpp_builder
 docker run --rm \
     --volume=`pwd`:/var/local/src/grpc:ro \
-    --volume=`pwd`/tools/dockerfile/benchmark/cpp_worker/build:/var/local/output \
+    --volume=`pwd`/tools/dockerfile/benchmark/cpp_builder/build:/var/local/output \
     cpp-builder
 
-cp tools/dockerfile/benchmark/cpp_worker/build tools/dockerfile/benchmark/qps_driver/build
+ln -s `pwd`/tools/dockerfile/benchmark/cpp_builder/build tools/dockerfile/benchmark/qps_driver/build
 docker build -t qps-driver tools/dockerfile/benchmark/qps_driver
+
+ln -s `pwd`/tools/dockerfile/benchmark/cpp_builder/build tools/dockerfile/benchmark/cpp_worker/build
+docker build -t cpp-worker tools/dockerfile/benchmark/cpp_worker
