85e61532b848a534ccd3eb166f96aede0ef35968
Lidi Zheng <lidiz@google.com>
2019-07-24 15:20:45 -0700

    bug fix

--- tools/run_tests/performance/k8s/run.sh
+++ tools/run_tests/performance/k8s/run.sh
@@ -14,7 +14,10 @@ docker run --rm --name cpp-worker-2 \
 python tools/run_tests/performance/k8s/gen_scenario.py > scenario.json
 
 docker run --rm --name the-qps-driver \
-    --volume `pwd`/scenario.json:/var/local/scenario.json \
+    --volume `pwd`/scenario.json:/var/local/scenario.json:ro \
+    --volume `pwd`:/var/local/result
     --network host \
     --env=QPS_WORKERS="localhost:10001,localhost:10002" \
-    qps-driver --scenarios_file=/var/local/scenario.json
+    qps-driver \
+    --scenarios_file=/var/local/scenario.json \
+    --scenario_result_file=/var/local/result/benchmark_result.json
