799ced6c66b2f422cdee52452619213ff89e6d18
Pau Freixes <pau.freixes@skyscanner.net>
2019-09-05 07:46:07 +0200

    [Aio] Unary unary client call barebones implementation
    Implement the minimal stuff for making a unary call with the new
    experimental gRPC Python implementation for Asyncio, called Aio.
    
    What has been added:
    
    - Minimal iomgr code for performing the required network and timer
    calls.
    - Minimal Cython code implementing the channel, call and the callback
    context.
    - Minimal Python code that mimics the synchronous implementation but
    designed to be asynchronous.
    
    Not solved yet:
    
    - Tests have to be executed using the `GRPC_ENABLE_FORK_SUPPORT=0`
    environment variable for skipping the fork handles installed by
    the core library.
    
    Co-authored-by: Manuel Miranda <manuel.miranda@skyscanner.net>
    Co-authored-by: Mariano Anaya <mariano.anaya@skyscanner.net>
    Co-authored-by: Zhanghui Mao <zhanghui.mao@skyscanner.net>

---
+++ src/python/grpcio_tests/tests_aio/unit/test_base.py
@@ -0,0 +1,58 @@
+# Copyright 2019 The gRPC Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import asyncio
+import unittest
+import socket
+
+from grpc.experimental import aio
+from tests_aio.unit import sync_server
+
+
+def _get_free_loopback_tcp_port():
+    if socket.has_ipv6:
+        tcp_socket = socket.socket(socket.AF_INET6)
+        host = "::1"
+        host_target = "[::1]"
+    else:
+        tcp_socket = socket.socket(socket.AF_INET)
+        host = "127.0.0.1"
+        host_target = host
+    tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    tcp_socket.bind((host, 0))
+    address_tuple = tcp_socket.getsockname()
+    return tcp_socket, "%s:%s" % (host_target, address_tuple[1])
+
+
+class AioTestBase(unittest.TestCase):
+
+    def setUp(self):
+        self._socket, self._target = _get_free_loopback_tcp_port()
+        self._server = sync_server.Server(self._target)
+        self._server.start()
+        self._loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self._loop)
+        aio.init_grpc_aio()
+
+    def tearDown(self):
+        self._server.terminate()
+        self._socket.close()
+
+    @property
+    def loop(self):
+        return self._loop
+
+    @property
+    def server_target(self):
+        return self._target
