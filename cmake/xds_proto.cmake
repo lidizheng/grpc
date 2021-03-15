6a6d0ee7124941d80a192791fb048a0b9b74333a
Lidi Zheng <lidiz@google.com>
2021-03-15 14:13:47 -0700

    Accommodate Windows' CMake behavior difference

--- cmake/xds_proto.cmake
+++ cmake/xds_proto.cmake
@@ -12,25 +12,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set(TEMPORARY_DIR ${CMAKE_BINARY_DIR}/xds_proto)
-file(MAKE_DIRECTORY ${TEMPORARY_DIR})
+set(_xDS_Proto_TEMPORARY_DIR ${CMAKE_BINARY_DIR}/xds_proto)
+file(MAKE_DIRECTORY ${_xDS_Proto_TEMPORARY_DIR})
 
-# This is bascially Bazel http_archive.
+# This is bascially Bazel's http_archive.
+# Note that strip_prefix strips the directory path prefix of the extracted
+# archive contect, and it may strip multiple directory.
 function(fetch_check_extract destination url hash strip_prefix)
   # Fetch and validate
-  set(TEMPORARY_FILE ${TEMPORARY_DIR}/${strip_prefix}.tar.gz)
-  file(DOWNLOAD ${url} ${TEMPORARY_FILE}
+  set(_xDS_Proto_TEMPORARY_FILE ${_xDS_Proto_TEMPORARY_DIR}/${strip_prefix}.tar.gz)
+  file(DOWNLOAD ${url} ${_xDS_Proto_TEMPORARY_FILE}
        TIMEOUT 60
        EXPECTED_HASH SHA256=${hash}
        TLS_VERIFY ON)
   # Extract
   execute_process(COMMAND
-                  ${CMAKE_COMMAND} -E tar xvf ${TEMPORARY_FILE}
-                  WORKING_DIRECTORY ${TEMPORARY_DIR}
+                  ${CMAKE_COMMAND} -E tar xvf ${_xDS_Proto_TEMPORARY_FILE}
+                  WORKING_DIRECTORY ${_xDS_Proto_TEMPORARY_DIR}
                   OUTPUT_QUIET)
-  file(MAKE_DIRECTORY ${destination})
-  file(RENAME ${TEMPORARY_DIR}/${strip_prefix} ${destination})
+  get_filename_component(_xDS_Proto_Destination_Path ${destination} DIRECTORY)
+  file(MAKE_DIRECTORY ${_xDS_Proto_Destination_Path})
+  file(RENAME ${_xDS_Proto_TEMPORARY_DIR}/${strip_prefix} ${destination})
   # Clean up
-  file(REMOVE ${TEMPORARY_DIR}/${strip_prefix})
-  file(REMOVE ${TEMPORARY_FILE})
+  file(REMOVE ${_xDS_Proto_TEMPORARY_DIR}/${strip_prefix})
+  file(REMOVE ${_xDS_Proto_TEMPORARY_FILE})
 endfunction()
